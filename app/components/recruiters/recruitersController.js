a698429e2b374a1bea43bc4b53317e0b21d1e9ed
Xavier <hayeck.xavier@gmail.com>
2016-03-03 19:41:10 -0800

    [feat] created view skeleton, updated html

---
+++ app/components/recruiters/recruitersController.js
@@ -0,0 +1,13 @@
+angular.module('evenHire.recruiterView', [])
+
+.controller('recruiterView', function($scope) {
+
+  $scope.findJobs = function() {
+
+  };
+
+  $scope.findApplicants = function() {
+
+  };
+
+});
