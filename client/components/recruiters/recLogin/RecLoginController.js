1e554255cf2db8b40d05796f1cd5754b655d2d5e
alicekao <alicekao.ak@gmail.com>
2016-03-05 15:52:54 -0800

    [feat]Added gulp test task

--- client/components/recruiters/recLogin/RecLoginController.js
+++ client/components/recruiters/recLogin/RecLoginController.js
@@ -2,13 +2,6 @@ angular.module('evenhire.recLogin', [])
 
   .controller('RecLoginController', ['$scope','$state', '$http', function ($scope, $state, $http) {
 
-    $scope.isEnter = function(envent, func, arg){
-    console.log("listening to keys");
-    if(envent.keyCode===13){
-      func.apply(null, arg);
-     }
-    };
-
     $scope.user = {};
     $scope.signIn = function() {
       return $http({
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,25 @@
 var gulp = require('gulp');
 //Include our plugins
-// var browserSync = require('browser-sync');
-// var reload = browserSync.create();
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
 var del = require('del');
-var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var Server = require('karma').Server;
 var uglify = require('gulp-uglify');
 
-// //Used to refresh the browser on a change, not used right now
-// gulp.task('browserSync', function() {
-//   browserSync({
-//     server: {
-//           baseDir: './dist/'
-//         },
-//     port: 8000,
-//     files: [
-//       'dist/**/*.*'
-//     ]
-//   });
-//   gulp.watch("./dist", reload);
+//Clean out the dist folder
+gulp.task('clean', function() {
+  return del(['client/dist/']);
+});
+
+//Lint js in client dir, not used
+// gulp.task('lint', function() {
+//   return gulp.src('./client/**/*.js')
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'));
 // });
-
-//Lint task (need to enter .js source)
-gulp.task('lint', function() {
-  return gulp.src('')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-//Compile our Sass into CSS (need to enter .scss source)
-gulp.task('styles', function() {
-  return gulp.src('./client/assets/styles/*.scss')
-    .pipe(sass())
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./client/dist/styles/'));
-});
 
 //Concatenante and minify JS
 gulp.task('scripts', function() {
@@ -51,29 +31,34 @@ gulp.task('scripts', function() {
     // .pipe(gulp.dest('./client/dist/'));
 });
 
-//Copys index.html file to dist folder
-// gulp.task('copy-html', function() {
-//   return gulp.src('./index.html')
-//     .pipe(gulp.dest('./dist/'));
-// });
-
-//Clean the dist folder, not used right now
-gulp.task('clean', function() {
-  return del(['client/dist/']);
-});
-
-// start our node server using nodemon
+//Start node server with nodemon
 gulp.task('serve', function() {
   nodemon({script: 'index.js', ignore: 'node_modules/**/*.js'});
 });
 
-//Runs these tasks everytime there is a change
+//Compile Sass into CSS
+gulp.task('styles', function() {
+  return gulp.src('./client/assets/styles/*.scss')
+    .pipe(sass())
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./client/dist/styles/'));
+});
+
+//Run tests
+gulp.task('tests', function(done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    }, done).start();
+});
+
+//Watch for changes in client folder
 gulp.task('watch', function() {
-  gulp.watch('./client/**/*.js', ['lint', 'scripts']);
+  gulp.watch(['./client/**/*.js', '!./client/dist/**/*.js'], ['scripts']);
   gulp.watch('./client/**/*.scss', ['styles']);
-  gulp.watch('index.html', ['copy-html']);
 });
 
 gulp.task('build', ['styles', 'scripts']);
-
-gulp.task('default', ['lint', 'styles', 'scripts', 'watch', 'serve']);
+gulp.task('test', ['tests']);
+gulp.task('default', ['styles', 'scripts', 'serve', 'watch']);
