c24229ce42108d2f820cd76735a5131bd0f1760b
Darko <darkogjorgoski@gmail.com>
2016-03-04 20:10:30 -0800

    [feat] Added http

--- client/components/applicants/appLogin/AppLoginController.js
+++ client/components/applicants/appLogin/AppLoginController.js
@@ -1,10 +1,19 @@
 angular.module('evenhire.appLogin', [])
 
-  .controller('AppLoginController', ['$scope','$state', function ($scope, $state) {
+  .controller('AppLoginController', ['$scope','$state','$http', function ($scope, $state, $http) {
 
     $scope.user = {};
     $scope.signIn = function() {
       console.log('trying to sign in!');
-      console.log($scope.user.username, $scope.user.password)
+      console.log($scope.user);
+
+    return $http({
+          method: 'POST',
+          url: '/api/applicants',
+          data: $scope.user
+            })
+      .then(function (data) {
+        console.log(data)
+      });
     };
 }]);
--- client/components/applicants/appNewAcc/AppNewAccController.js
+++ client/components/applicants/appNewAcc/AppNewAccController.js
@@ -1,19 +1,25 @@
 angular.module('evenhire.appNewAcc', [])
 
-  .controller('AppNewAccController', ['$scope', '$state', function ($scope, $state) {
+  .controller('AppNewAccController', ['$scope', '$state','$http', function ($scope, $state, $http) {
 
-    $scope.applicant = {
-      firstName: 'example1',
-      lastName: 'example2',
-      username: 'example3',
-      email: 'example4',
-      password: 'example5'
-    };
+    $scope.applicant = {};
 
     $scope.sendApplicantInfo = function() {
       //send $scope.company to router
       console.log('inside createAccount controller');
+      console.log($scope.applicant);
+
+      return $http({
+      method: 'POST',
+      url: 'api/applicants',
+      data: $scope.applicant
+      }).then(function successCallback(response) {
+        console.log(response);
+      }, function errorCallback(response) {
+        console.log(response)
+      });
+
     };
-    
+
   }]);
 
