aeb72dc465ce52dc1904fa0ea14483091be94aaa
alicekao <alicekao.ak@gmail.com>
2016-03-08 11:16:58 -0800

    [feat] Added basic unit testing of 2 controllers

--- client/components/recruiters/recNewAcc/test.js
+++ client/components/recruiters/recNewAcc/test.js
@@ -1,26 +1,35 @@
-// describe('Creation', function() {
+describe('RecNewAccController', function() {
+  // beforeEach(module('ui.router'));
+  // beforeEach(module('evenhire.recNewAcc'));
+  var scope;
+  var createController;
+  var httpBackend;
+  var $state;
+  // var $controller;
+  // var $httpBackend;
+  beforeEach(module('evenhire'));
+  beforeEach(inject(function($rootScope, $controller, $httpBackend) {
+    scope = $rootScope.$new();
+    httpBackend = $httpBackend;
 
+    createController = function() {
+      return $controller('RecNewAccController', {
+        $scope: scope,
+        $http: httpBackend
+      });
+    };
+    createController();
+  }));
 
-//   beforeEach(module('ui.router'));
-//   beforeEach(module('evenhire.recNewAcc'));
+  it('should have a createRecAccount fn', function() {
+    expect(scope.createRecAcc).toBeDefined();
+  });
 
+  it('1 + 1 should equal 2', function() {
+    scope.x = 1;
+    scope.y = 2;
+    scope.sum();
+    expect(scope.z).toBe(3);
+  });
 
-
-//   var $controller;
-
-//   beforeEach(inject(function(_$controller_) {
-//     $controller = _$controller_;
-//   }));
-
-//   describe('sum', function() {
-//     it('1 + 1 should equal 2', function() {
-//       var $scope = {};
-//       var controller = $controller('RecNewAccController', {$scope: $scope});
-//       $scope.x = 1;
-//       $scope.y = 2;
-//       $scope.sum();
-//       expect($scope.z).toBe(3);
-//     });
-//   });
-
-// });
+});
