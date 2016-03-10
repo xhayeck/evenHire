d52dde3eeca296ffb9c58a4b8e8bc2a35152d5b9
alicekao <alicekao.ak@gmail.com>
2016-03-10 13:42:39 -0800

    [feat] Added authorization limitation

--- client/components/home/HomeController.js
+++ client/components/home/HomeController.js
@@ -2,12 +2,5 @@
 angular.module('evenhire.home', [])
 
   .controller('HomeController', ['$scope','$state', 'Applicant', function ($scope, $state, Applicant) {
-    $scope.testFunc = function() {
-      console.log('inside home controller');
-    };
-    $scope.logOut = function() {
-      Applicant.signOut();
-      console.log('signed out');
-      $state.go('appLogin');
-    };
+    //Add functions we will want to run on the home page
 }]);
