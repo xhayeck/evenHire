024c3654a9be472fe073082e9bde6fc5fc542dd3
Thomas Sorensen <thomaspsorensen@gmail.com>
2016-03-12 11:43:20 -0800

    [bug] Fixed error - now checks for login

--- client/components/applicants/allJobs/AllJobsController.js
+++ client/components/applicants/allJobs/AllJobsController.js
@@ -18,7 +18,12 @@ angular.module('evenhire.allJobs', [])
 
       Applicant.apply({job_id: job_id})
         .then(function(factoryResponse) {
-          if(!factoryResponse) {
+          console.log("factoryResponse in alljobsController", factoryResponse);
+          if(factoryResponse.status === 500){
+            console.log("You need to login");
+            $state.go('appLogin')
+          }
+          else if(!factoryResponse) {
             alert("You already applied for that job")
           } else {
             alert("Thanks for applying " + factoryResponse.first_name)
