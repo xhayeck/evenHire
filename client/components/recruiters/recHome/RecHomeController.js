0490f06aba743716efe3168512c3590987b1debc
alicekao <alicekao.ak@gmail.com>
2016-03-15 16:42:11 -0700

    [feat] Updated job schema to add city, state zipcode

--- client/components/recruiters/recHome/RecHomeController.js
+++ client/components/recruiters/recHome/RecHomeController.js
@@ -9,6 +9,11 @@ angular.module('evenhire.recruiters', [])
   $scope.companyName = currentUser.name;
   $scope.contactMessage = 'We\'d like to schedule an interview!';
   $scope.companyEmail = currentUser.email;
+  $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });
 
 
   $scope.clickToOpen = function () {
--- server/db/models.js
+++ server/db/models.js
@@ -79,7 +79,9 @@ module.exports = function(db) {
 
   var Job = db.define('jobs', {
     title: Sequelize.STRING,
-    location: Sequelize.STRING,
+    city: Sequelize.STRING,
+    state: Sequelize.STRING,
+    zipcode: Sequelize.STRING,
     description: Sequelize.TEXT,
     industry: Sequelize.STRING,
     career_level: Sequelize.STRING,
@@ -99,7 +101,7 @@ module.exports = function(db) {
   Applicant.belongsToMany(Job, {through: 'jobs_applicants'});
 
   //WARNING! to sync and possibly clear db uncomment this line:
-  //db.sync({force: true});
+  // db.sync({force: true});
 
   return {
     Job: Job,
