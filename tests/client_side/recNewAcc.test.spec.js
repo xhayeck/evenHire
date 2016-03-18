e057504f4656057c83621248201c60aeabb84f54
Xavier <hayeck.xavier@gmail.com>
2016-03-17 17:16:21 -0700

    [feat] Created recruiter test framework. They work

---
+++ tests/client_side/recNewAcc.test.spec.js
@@ -0,0 +1,46 @@
+describe('RecNewAccController', function(){
+
+  var $scope;
+  var httpBackend;
+  var $state;
+  var Recruiter;
+  var $controller
+
+  //load module
+  beforeEach(module('evenhire'));
+  //inject services for testing
+  beforeEach(inject(function ($injector) {
+    $rootScope = $injector.get('$rootScope');
+    httpBackend = $injector.get('$httpBackend');
+    $state = $injector.get('$state');
+    Recruiter = $injector.get('Recruiter');
+    $controller = $injector.get('$controller');
+    $scope = $rootScope.$new();
+
+    createController = function() {
+      return $controller('RecNewAccController', {
+        $scope: $scope,
+        $state: $state,
+        Recruiter: Recruiter
+      });
+    };
+
+      httpBackend.whenGET('api/recruiter/getPostedJobs').respond(200, {data: ['job1', 'job2', 'job3']});
+      httpBackend.whenGET('components/home/homeView.html').respond('<div>mock template</div>');
+      createController();
+
+  }));
+
+  afterEach(function() {
+    httpBackend.flush();
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('Creation of Recruiter Account', function() {
+    it('should have a createRecAcc', function() {
+      expect($scope.createRecAcc).to.exist;
+    });
+  });
+
+});
