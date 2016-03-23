2d188d91a75b8fb67b3c7e0ef0e92cfa0f5cff61
alicekao <alicekao.ak@gmail.com>
2016-03-22 19:36:19 -0700

    [style] Added hero image

--- client/app.js
+++ client/app.js
@@ -20,7 +20,7 @@ angular.module('evenhire',[
 
   .config(function($stateProvider, $urlRouterProvider, $httpProvider, $mdThemingProvider) {
     $mdThemingProvider.theme('default')
-      .primaryPalette('amber');
+      .primaryPalette('indigo');
     // .backgroundPalette('grey');
       // .dark();
     $urlRouterProvider.otherwise('/');
