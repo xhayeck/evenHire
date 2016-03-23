1ab77c473cdf2ecc221f494073a57b3adb07753e
Darko <darkogjorgoski@gmail.com>
2016-03-22 19:07:35 -0700

    [feat] Added resume check for first and last name

--- client/components/applicants/appNewAcc/AppNewAccController.js
+++ client/components/applicants/appNewAcc/AppNewAccController.js
@@ -4,30 +4,26 @@ angular.module('evenhire.appNewAcc', [])
 
     $scope.applicant = {};
     $scope.states = Home.states;
+    $scope.resumeCheck = false;
 
     $scope.createAccount = function() {
-      //send form data to the server at api/applicant/signUp
       if ($scope.applicant.resume.match($scope.applicant.firstName) || $scope.applicant.resume.match($scope.applicant.lastName)) {
-      return ngDialog.open({
-          template: './components/applicants/appNewAcc/appErrorWithResume.tmpl.html',
-          controller: 'AppNewAccController',
-          className: 'ngdialog-theme-default',
-          scope: $scope
+      $scope.resumeCheck = true;
+      } else {
+        Auth.signUp($scope.applicant, 'applicant')
+        .then(function(data) {
+          if (!data.type) {
+            console.log('User already exists', data.data);
+          } else {
+            $window.localStorage.setItem('evenhire', data.token);
+            console.log('NEW USER is :', data);
+            $state.go('allJobs');
+          }
         });
-      }
-      Auth.signUp($scope.applicant, 'applicant')
-      .then(function(data) {
-        if (!data.type) {
-          console.log('User already exists', data.data);
-        } else {
-          $window.localStorage.setItem('evenhire', data.token);
-          console.log('NEW USER is :', data);
-          $state.go('allJobs');
-        }
-      });
+      };
     };
-
   }])
+
   .directive('valueMatches', ['$parse', function ($parse) {
     return {
       require: 'ngModel',
